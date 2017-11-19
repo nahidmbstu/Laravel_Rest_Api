143f4a3f710272db48eeb2ab080f9d8959123491
nahidMBSTU <nahid.mbstu.ict@gmail.com>
2017-11-19 16:50:45 +0600

    articles authorization

--- routes/api.php
+++ routes/api.php
@@ -10,41 +10,34 @@ Route::post('register', 'API\UserController@register');
 
 Route::group(['middleware' => 'auth:api'], function(){
     Route::post('details', 'API\UserController@details');
-
-
     
 });
-
-
-
-
-
 
 
 Route::get('articles', function() {
     // If the Content-Type and Accept headers are set to 'application/json', 
     // this will return a JSON structure. This will be cleaned up later.
     return Article::all();
-});
+})->middleware('auth:api');
  
 Route::get('articles/{id}', function($id) {
     return Article::find($id);
-});
+})->middleware('auth:api');
 
 Route::post('articles', function(Request $request) {
     return Article::create($request->all);
-});
+})->middleware('auth:api');
 
 Route::put('articles/{id}', function(Request $request, $id) {
     $article = Article::findOrFail($id);
     $article->update($request->all());
 
     return $article;
-});
+})->middleware('auth:api');
 
 Route::delete('articles/{id}', function($id) {
     Article::find($id)->delete();
 
     return 204;
-});
+})->middleware('auth:api');
 
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::get('/', function () {
 });
 
 
-Route::get('articles', 'ArticleController@index');
-Route::get('articles/{id}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{id}', 'ArticleController@update');
-Route::delete('articles/{id}', 'ArticleController@delete');
+// Route::get('articles', 'ArticleController@index');
+// Route::get('articles/{id}', 'ArticleController@show');
+// Route::post('articles', 'ArticleController@store');
+// Route::put('articles/{id}', 'ArticleController@update');
+// Route::delete('articles/{id}', 'ArticleController@delete');
